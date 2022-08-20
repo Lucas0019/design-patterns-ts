52f06fac97c9d19cc605b9509d277a2b2cf659b9
Lucas Xavier <lxvier.00@outlook.com>
2022-08-20 18:50:07 -0300

    feat: renamed study folder

---
+++ solid/liskov-substitution-principle/src/index.ts
@@ -0,0 +1,5 @@
+// lsp - liskov substitution principle
+
+// Quando es­ten­dendo uma classe, lem­bre-se que você deve ser capaz de pas­sar 
+// ob­je­tos da sub­classe em lugar de ob­je­tos da classe mãe sem que­brar o có­digo cliente.
+
